fa6989ea438ebbdf4a40027915f28430370c65b7
Miguel Salinas <miguelasalinass@utexas.edu>
2022-05-22 14:06:38 -0500

    adding more tools

--- install_tools.sh
+++ install_tools.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/sh
 
 
-sudo apt install tmux vim tilda zsh docker fonts-powerline curl htop
+sudo apt install tmux vim tilda zsh docker fonts-powerline curl htop chromium-browser
 sudo snap install --classic code # or code-insiders
 
-echo "imap jk <esc> \n set number"  >> ~/.vimrc 
+echo "imap jk <esc> \n set number \n iset hlsearch"  >> ~/.vimrc 
 
 
-echo "You will manually have to set the theme to agnoster inside ~/.zshrc"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Setting agnoster to default ZSH theme"
 
 cat zshrc >> ~/.zshrc
 cat bash_aliases >> ~/.bash_aliases
+sed -i "/ZSH_THEME/c\ZSH_THEME=\"agnoster\";d" ~/.zshrc
+mkdir -p ~/.config/tilda
+cp tilda_config ~/.config/tilda/tilda_0
